9b457c24c345ac480669cbe4d443d4f9877b5699
Konstantin Ananev <ananevkv@yoomoney.ru>
2022-10-15 13:10:38 +0300

    Counter as separate file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from "react";
+import Counter from "./components/Counter";
 
 function App() {
   const [countName, setCountName] = React.useState('Имя счетчика')
   const [likes, setLikes] = React.useState(0);
-  
-  function increment() {
-    setLikes(likes + 1)
-  }
-
-  function decrement() {
-    setLikes(likes - 1)
-  }
 
   return (
     <div className="App">
-      <h1>{countName}</h1>
-      <h1>{likes}</h1>
-      <input type = 'text' value={countName} onChange={event => setCountName(event.target.value)}></input>
-
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
     </div>
   );
 }
