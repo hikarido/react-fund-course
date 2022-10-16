55794a4c8fbeb330c56b0bbb7d189973bb613072
Konstantin Ananev <ananevkv@yoomoney.ru>
2022-10-16 11:17:27 +0300

    Styles were used + props passed to component. Finished on arrays usage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import CounterAsClass from "./components/CounterAsClass";
+import './styles/App.css';
+import PostItem from "./components/PostItem";
 
 function App() {
   const [countName, setCountName] = React.useState('Имя счетчика')
@@ -7,7 +9,20 @@ function App() {
 
   return (
     <div className="App">
-      <CounterAsClass/>
+      <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>      
+      <PostItem post={{id: 2, title: 'JavaScript', body: 'Description'}}/>      
+      <PostItem post={{id: 3, title: 'JavaScript', body: 'Description'}}/>      
+      <PostItem post={{id: 4, title: 'JavaScript', body: 'Description'}}/>      
+      <PostItem post={{id: 5, title: 'JavaScript', body: 'Description'}}/>      
+      <PostItem post={{id: 6, title: 'JavaScript', body: 'Description'}}/>      
+      <PostItem post={{id: 7, title: 'JavaScript', body: 'Description'}}/>      
+      <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>      
+      <PostItem post={{id: 2, title: 'JavaScript', body: 'Description'}}/>      
+      <PostItem post={{id: 3, title: 'JavaScript', body: 'Description'}}/>      
+      <PostItem post={{id: 4, title: 'JavaScript', body: 'Description'}}/>      
+      <PostItem post={{id: 5, title: 'JavaScript', body: 'Description'}}/>      
+      <PostItem post={{id: 6, title: 'JavaScript', body: 'Description'}}/>      
+      <PostItem post={{id: 7, title: 'JavaScript', body: 'Description'}}/>      
     </div>
   );
 }
