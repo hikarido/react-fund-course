6d57b03b6171583d481c8dfa26aec8fff027a3fb
Konstantin Ananev <ananevkv@yoomoney.ru>
2022-10-16 11:46:35 +0300

    convertation of array to list of divs

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import CounterAsClass from "./components/CounterAsClass";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
 
 function App() {
-  const [countName, setCountName] = React.useState('Имя счетчика')
-  const [likes, setLikes] = React.useState(0);
+  const [posts, setPosts] = useState([
+    {id: 1, tytle: "Java Script", body: "Java script это яхык программирования"},
+    {id: 2, tytle: "Java Script", body: "Java script это яхык программирования"},
+    {id: 3, tytle: "Java Script", body: "Java script это яхык программирования"}    
+  ]);
+
+  console.log(posts);
 
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>      
-      <PostItem post={{id: 2, title: 'JavaScript', body: 'Description'}}/>      
-      <PostItem post={{id: 3, title: 'JavaScript', body: 'Description'}}/>      
-      <PostItem post={{id: 4, title: 'JavaScript', body: 'Description'}}/>      
-      <PostItem post={{id: 5, title: 'JavaScript', body: 'Description'}}/>      
-      <PostItem post={{id: 6, title: 'JavaScript', body: 'Description'}}/>      
-      <PostItem post={{id: 7, title: 'JavaScript', body: 'Description'}}/>      
-      <PostItem post={{id: 1, title: 'JavaScript', body: 'Description'}}/>      
-      <PostItem post={{id: 2, title: 'JavaScript', body: 'Description'}}/>      
-      <PostItem post={{id: 3, title: 'JavaScript', body: 'Description'}}/>      
-      <PostItem post={{id: 4, title: 'JavaScript', body: 'Description'}}/>      
-      <PostItem post={{id: 5, title: 'JavaScript', body: 'Description'}}/>      
-      <PostItem post={{id: 6, title: 'JavaScript', body: 'Description'}}/>      
-      <PostItem post={{id: 7, title: 'JavaScript', body: 'Description'}}/>      
+      {posts.map(post =>
+        <div key={post.id}>
+          <PostItem post={post}/>
+        </div>  
+      )}
     </div>
   );
 }
