5f9114183cc3bbcf3cc4ebc77a557efd3a13166a
Konstantin Ananev <ananevkv@yoomoney.ru>
2022-11-03 10:20:47 +0300

    свои компоненты +управляемый и не управляемый компонент пример

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import CounterAsClass from "./components/CounterAsClass";
 import './styles/App.css';
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
-    {id: 1, tytle: "Java Script", body: "Java script это язык программирования"},
-    {id: 2, tytle: "Java Script", body: "Java script это язык программирования"},
-    {id: 3, tytle: "Java Script", body: "Java script это язык программирования"}    
+    { id: 1, tytle: "Java Script", body: "Java script это язык программирования" },
+    { id: 2, tytle: "Java Script", body: "Java script это язык программирования" },
+    { id: 3, tytle: "Java Script", body: "Java script это язык программирования" }
   ]);
 
   const [postsAboutJava, setPostsAboutJava] = useState([
-    {id: 1, tytle: "Java", body: "Java это язык программирования"},
-    {id: 2, tytle: "Java", body: "Java это язык программирования"},
-    {id: 3, tytle: "Java", body: "Java это язык программирования"}    
+    { id: 1, tytle: "Java", body: "Java это язык программирования" },
+    { id: 2, tytle: "Java", body: "Java это язык программирования" },
+    { id: 3, tytle: "Java", body: "Java это язык программирования" }
   ]);
+
+  const [title, setTitle] = useState('')
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    // console.log(title)
+    console.log(bodyInputRef.current.value)
+    console.log(bodyInputRef.current)
+  }
+
+  const bodyInputRef = useRef();
 
   return (
     <div className="App">
-      <PostList posts={posts} title="Список постов о javaScript"/>
-      <PostList posts={postsAboutJava} title="Список постов о java"/>
+      <form>
+        // управляемый инпет
+        <MyInput
+          value={title}
+          type="text"
+          onChange={e => setTitle(e.target.value)}
+          placeholder="Название поста" />
+        {/* <MyInput
+          ref={bodyInputRef}
+          type="text"
+          placeholder="Описание поста" /> */}
+        
+        // не управляемый компонент 
+        <input ref={bodyInputRef} type="text"/>
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Список постов о javaScript" />
+      <PostList posts={postsAboutJava} title="Список постов о java" />
     </div>
   );
 }
